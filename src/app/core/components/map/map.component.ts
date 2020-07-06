3d785b8d721132918263c0ec1e427a9ef619f93c
asuarezc91 <angeldaniel4@hotmail.com>
2020-07-06 20:12:01 +0200

    the best map formation, point to RESTORE

--- src/app/core/components/map/map.component.ts
+++ src/app/core/components/map/map.component.ts
@@ -7,10 +7,15 @@ import { escapeRegExp } from '@angular/compiler/src/util';
 import { hasUncaughtExceptionCaptureCallback } from 'process';
 //ESRI
 // mapView es la view en este caso!! 
-import Map from "esri/Map"
+import Map from "esri/Map";
+// import * as Map from 'esri/Map'; 
+
 import request from "esri/request"
+
 import esriConfig from "esri/config"
-import Field from  "esri/layers/support/Field"
+
+import Field from "esri/layers/support/Field"
+
 
 
 
@@ -24,7 +29,7 @@ export class MapComponent {
   // Get a container link for map place
   @ViewChild('mapView', { static: true }) private readonly mapViewElement: ElementRef;
   // main map view
-  private mapView;
+  public mapView;
   title = 'ArcGIS angular map';
 
   mapping: any;
@@ -58,9 +63,6 @@ export class MapComponent {
         };
         // create map view by default properties
         this.mapView = new MapView(mapViewProperties);
-
-  
-
         // Set service properties
         // url - this address to MapServer from ArcGIS Enterprise
         // sublayers - this are the settings for the inner layers of the service.
@@ -73,9 +75,9 @@ export class MapComponent {
         const oilSandsLayer = new MapImageLayer(oilSandLayerProperties);
         // Adding a layer into map
         map.add(oilSandsLayer);
-        
 
- 
+
+
 
       });
 
@@ -87,121 +89,107 @@ export class MapComponent {
   ngOnInit() {
     this.dataApi.reservation$.subscribe(data => {
       console.log(data);
+      console.log(this.mapView.spatialReference);
       // this.generateFeatureCollection(data);
     })
-
   }
 
 
-  // man() {
-  //   const test = new Map ();
-  //   console.log(test); 
-  // }
+  
+      
+//   generateFeatureCollection(fileName) {
+//     var name = fileName.split(".");
+//     // Chrome and IE add c:\fakepath to the value - we need to remove it
+//     // see this link for more info: http://davidwalsh.name/fakepath
+//     name = name[0].replace("c:\\fakepath\\", "");
+
+//     document.getElementById("upload-status").innerHTML =
+//       "<b>Loading </b>" + name;
+
+//     // define the input params for generate see the rest doc for details
+//     // https://developers.arcgis.com/rest/users-groups-and-items/generate.htm
+//     var params = {
+//       name: name,
+//       targetSR: this.mapView.spatialReference,
+//       maxRecordCount: 1000,
+//       enforceInputFileSizeLimit: true,
+//       enforceOutputJsonSizeLimit: true
+//     };
+
+
+//     // generalize features to 10 meters for better performance
+//     // params.generalize = true;
+//     // params.maxAllowableOffset = 10;
+//     // params.reducePrecision = true;
+//     // params.numberOfDigitsAfterDecimal = 0;
+
+//     var myContent = {
+//       filetype: "shapefile",
+//       publishParameters: JSON.stringify(params),
+//       f: "json"
+//     };
+
+//     let portalUrl = "https://www.arcgis.com";
+
+//     let options = {
+//       query: myContent,
+//       body: document.getElementById("uploadForm"),
+//       responseType: "json"
+//     }
+
+//     // use the REST generate operation to generate a feature collection from the zipped shapefile
+//     request(portalUrl + "/sharing/rest/content/features/generate", options)
+//       .then(function (response) {
+//         var layerName =
+//           response.data.featureCollection.layers[0].layerDefinition.name;
+//         document.getElementById("upload-status").innerHTML =
+//           "<b>Loaded: </b>" + layerName;
+//         addShapefileToMap(response.data.featureCollection);
+//       })
+//       .catch(errorHandler);
+//   }
 
 
 
+//   function errorHandler(error) {
+//     document.getElementById("upload-status").innerHTML =
+//       "<p style='color:red;max-width: 500px;'>" + error.message + "</p>";
+//   }
 
+//   function addShapefileToMap(featureCollection) {
+//     // add the shapefile to the map and zoom to the feature collection extent
+//     // if you want to persist the feature collection when you reload browser, you could store the
+//     // collection in local storage by serializing the layer using featureLayer.toJson()
+//     // see the 'Feature Collection in Local Storage' sample for an example of how to work with local storage
+//     var sourceGraphics = [];
 
+//     var layers = featureCollection.layers.map(function (layer) {
+//       var graphics = layer.featureSet.features.map(function (feature) {
+//         return Graphic.fromJSON(feature);
+//       });
+//       sourceGraphics = sourceGraphics.concat(graphics);
+//       var featureLayer = new FeatureLayer({
+//         objectIdField: "FID",
+//         source: graphics,
+//         fields: layer.layerDefinition.fields.map(function (field) {
+//           return Field.fromJSON(field);
+//         })
+//       });
+//       return featureLayer;
+//       // associate the feature with the popup on click to enable highlight and zoom to
+//     });
+//     map.addMany(layers);
+//     view.goTo(sourceGraphics).catch(function (error) {
+//       if (error.name != "AbortError") {
+//         console.error(error);
+//       }
+//     });
 
+//     document.getElementById("upload-status").innerHTML = "";
+//   }
+// });
 
-
-
-
-  //       generateFeatureCollection(fileName) {
-  //       var name = fileName.split(".");
-  //       // Chrome and IE add c:\fakepath to the value - we need to remove it
-  //       // see this link for more info: http://davidwalsh.name/fakepath
-  //       name = name[0].replace("c:\\fakepath\\", "");
-
-  //       document.getElementById("upload-status").innerHTML =
-  //         "<b>Loading </b>" + name;
-
-  //       // define the input params for generate see the rest doc for details
-  //       // https://developers.arcgis.com/rest/users-groups-and-items/generate.htm
-  //       var params = {
-  //         name: name,
-  //         targetSR: this.mapView.spatialReference,
-  //         maxRecordCount: 1000,
-  //         enforceInputFileSizeLimit: true,
-  //         enforceOutputJsonSizeLimit: true
-  //       };
-
-
-  //       // generalize features to 10 meters for better performance
-  //       // params.generalize = true;
-  //       // params.maxAllowableOffset = 10;
-  //       // params.reducePrecision = true;
-  //       // params.numberOfDigitsAfterDecimal = 0;
-
-  //       var myContent = {
-  //         filetype: "shapefile",
-  //         publishParameters: JSON.stringify(params),
-  //         f: "json"
-  //       };
-
-  //       let portalUrl = "https://www.arcgis.com";
-
-  //       let options = {
-  //         query: myContent,
-  //         body: document.getElementById("uploadForm"),
-  //         responseType: "json"
-  //       }
-
-  //       // use the REST generate operation to generate a feature collection from the zipped shapefile
-  //       request(portalUrl + "/sharing/rest/content/features/generate", options)
-  //         .then(function (response) {
-  //           var layerName =
-  //             response.data.featureCollection.layers[0].layerDefinition.name;
-  //           document.getElementById("upload-status").innerHTML =
-  //             "<b>Loaded: </b>" + layerName;
-  //           addShapefileToMap(response.data.featureCollection);
-  //         })
-  //         .catch(errorHandler);
-  //     }
-
-
-
-  //     function errorHandler(error) {
-  //       document.getElementById("upload-status").innerHTML =
-  //         "<p style='color:red;max-width: 500px;'>" + error.message + "</p>";
-  //     }
-
-  //     function addShapefileToMap(featureCollection) {
-  //       // add the shapefile to the map and zoom to the feature collection extent
-  //       // if you want to persist the feature collection when you reload browser, you could store the
-  //       // collection in local storage by serializing the layer using featureLayer.toJson()
-  //       // see the 'Feature Collection in Local Storage' sample for an example of how to work with local storage
-  //       var sourceGraphics = [];
-
-  //       var layers = featureCollection.layers.map(function (layer) {
-  //         var graphics = layer.featureSet.features.map(function (feature) {
-  //           return Graphic.fromJSON(feature);
-  //         });
-  //         sourceGraphics = sourceGraphics.concat(graphics);
-  //         var featureLayer = new FeatureLayer({
-  //           objectIdField: "FID",
-  //           source: graphics,
-  //           fields: layer.layerDefinition.fields.map(function (field) {
-  //             return Field.fromJSON(field);
-  //           })
-  //         });
-  //         return featureLayer;
-  //         // associate the feature with the popup on click to enable highlight and zoom to
-  //       });
-  //       map.addMany(layers);
-  //       view.goTo(sourceGraphics).catch(function (error) {
-  //         if (error.name != "AbortError") {
-  //           console.error(error);
-  //         }
-  //       });
-
-  //       document.getElementById("upload-status").innerHTML = "";
-  //     }
-  //   });
-
-  // }
-
-
+// }
 
 
 
